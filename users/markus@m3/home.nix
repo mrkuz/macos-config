8273ddc01c28422cea45af5be4255fd917201ac3
mrkuz <markus@bitsandbobs.net>
2023-10-11 19:28:39 +0200

    Move Brewfile

--- users/markus@m3/home.nix
+++ users/markus@m3/home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   home.sessionVariables = {
-    HOMEBREW_BUNDLE_FILE = "/Users/markus/etc/config.git/misc/Brewfile";
+    HOMEBREW_BUNDLE_FILE = "/Users/markus/etc/config.git/var/Brewfile";
     SHELL = "${pkgs.fish}/bin/fish";
   };
 
