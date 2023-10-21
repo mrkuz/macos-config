1ab8c81059b240d9e18f2b6e525d3353fca52acd
mrkuz <markus@bitsandbobs.net>
2023-10-21 20:07:22 +0200

    Add some color to go.sh

--- go.sh
+++ go.sh
@@ -9,6 +9,26 @@ fi
 
 KEEP_GENERATIONS="1"
 
+# Colors
+
+BLACK="\e[0;30m"
+RED="\e[0;31m"
+GREEN="\e[0;32m"
+YELLOW="\e[0;33m"
+BLUE="\e[0;34m"
+MAGENTA="\e[0;35m"
+CYAN="\e[0;36m"
+LIGHT_GRAY="\e[0;37m"
+DARK_GRAY="\e[0;90m"
+LIGHT_RED="\e[0;91m"
+LIGHT_GREEN="\e[0;92m"
+LIGHT_YELLOW="\e[0;93m"
+LIGHT_BLUE="\e[0;94m"
+LIGHT_MAGENTA="\e[0;95m"
+LIGHT_CYAN="\e[0;96m"
+WHITE="\e[0;97m"
+RESET="\e[0m"
+
 function usage() {
     echo "
 Usage: $0 COMMAND
@@ -23,30 +43,52 @@ Commands:
     exit 1
 }
 
+function info() {
+    echo -e "\n${BLUE}>>>${RESET} $1\n"
+}
+
+function pause() {
+    echo -e "\n${YELLOW}>>>${RESET} Press any key to continue..."
+    read -n1    
+}
+
 function update() {
+    info "Updating flake inputs"
     nix flake update
     git diff flake.lock
+    pause
 
+    info "Updating brew"
     brew update
-    brew outdated --greedy
+    pause
 
+    info "Outdated brew packages"
+    brew outdated --greedy
+    pause
+
+    info "Outdated Mac App Store packages"
     mas outdated
+    pause
 }
 
 function rebuild() {
+    info "Rebuild and switch"
     darwin-rebuild switch -v --flake ".#$1"
 }
 
 function upgrade() {
     rebuild "$1"
-    
+
+    info "Upgrading brew packages"
     brew upgrade --greedy
     brew bundle dump -f
 
+    info "Upgrading Mac App Store packages"
     mas upgrade
 }
 
 function clean() {
+    info "Deleting up old generations"
     HOME=/var/root sudo nix-env --delete-generations +"$KEEP_GENERATIONS" --profile /nix/var/nix/profiles/system
 
     for path in /Users/*; do
@@ -55,11 +97,15 @@ function clean() {
             HOME=/var/root sudo nix-env --delete-generations +"$KEEP_GENERATIONS" --profile "/Users/$user/.local/state/nix/profiles/home-manager"
         fi
     done
-    
-    HOME=/var/root sudo nix-collect-garbage
+    pause
 
-    echo
+    info "Collecting garbate"
+    HOME=/var/root sudo nix-collect-garbage
+    pause
+
+    info "Nix store roots"
     nix-store --gc --print-roots | grep -v {censored} | column -t | sort -k3 -k1
+    pause
 }
 
 case "$1" in
--- users/markus@m3/home.nix
+++ users/markus@m3/home.nix
@@ -35,6 +35,7 @@
       jq
       ncdu
       pdftk
+      pstree
       pwgen
       rclone
       ripgrep
