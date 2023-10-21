53c9773b82a32db44834f272ae92942890d6bcd0
mrkuz <markus@bitsandbobs.net>
2023-10-21 19:51:08 +0200

    Add vterm to emacs

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -14,7 +14,7 @@ in
   config = mkIf cfg.enable {
     home = {
       packages = with pkgs; [
-        emacs-unstable
+        ((emacsPackagesFor emacs-unstable).emacsWithPackages (epkgs: [ epkgs.vterm ]))
       ];
     };
 
