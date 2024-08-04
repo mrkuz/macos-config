0d360073e5c15891b3075a9e667b91d7f49bae8f
mrkuz <markus@bitsandbobs.net>
2024-08-04 14:18:00 +0200

    Switch from pantheon to gnome (toolbox-ui vm)

--- hosts/nixos/vm/toolbox-ui.nix
+++ hosts/nixos/vm/toolbox-ui.nix
@@ -4,12 +4,12 @@
     nix.enable = true;
     minimize.enable = false;
     qemuGuest = {
-      autoLogin = true;
+      # autoLogin = true;
       dhcp = true;
       graphics = true;
+      opengl = true;
       socketVmnet = true;
       user = vars.primaryUser;
-      opengl = true;
     };
   };
 
@@ -19,10 +19,14 @@
     nixos.enable = false;
   };
 
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+  ];
+
   environment.systemPackages = with pkgs; [
-    pantheon.elementary-terminal
     # Base utils
     file
+    htop
     ripgrep
     # Development
     firefox-devedition
@@ -30,12 +34,15 @@
   ];
 
   programs.fish.enable = true;
-  services.pantheon.apps.enable = false;
+  programs.gnome-terminal.enable = true;
+  services.gnome.core-utilities.enable = false;
+  services.gnome.evolution-data-server.enable = lib.mkForce false;
 
   services.xserver = {
     enable = true;
     excludePackages = [ pkgs.xterm ];
-    desktopManager.pantheon.enable = true;
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
   };
 
   users.users."${vars.primaryUser}" = {
