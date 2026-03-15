4565cba72decb58c02eee300a3ffc0ec8d2253f5
mrkuz <markus@bitsandbobs.net>
2026-03-15 18:32:24 +0100

    Remove tuptime module

--- flake.nix
+++ flake.nix
@@ -5,21 +5,43 @@
     # nixpkgs-master.url = "github:nixos/nixpkgs/master";
     nixos-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-    nix-alien = { url = "github:thiagokokada/nix-alien"; inputs.nixpkgs.follows = "nixpkgs-unstable"; };
-    nix-darwin = { url = "github:LnL7/nix-darwin/master"; inputs.nixpkgs.follows = "nixpkgs-unstable"; };
-    nix-homebrew = { url = "github:zhaofengli-wip/nix-homebrew"; inputs.nixpkgs.follows = "nixpkgs-unstable"; };
-    nix-snapd = { url = "github:nix-community/nix-snapd"; inputs.nixpkgs.follows = "nixpkgs-unstable"; };
-    home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs-unstable"; };
-    emacs-overlay = { url = "github:nix-community/emacs-overlay"; inputs.nixpkgs.follows = "nixpkgs-unstable"; };
-    apple-silicon = { url = "github:tpwrules/nixos-apple-silicon"; inputs.nixpkgs.follows = "nixpkgs-unstable"; };
+    nix-alien = {
+      url = "github:thiagokokada/nix-alien";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    nix-homebrew = {
+      url = "github:zhaofengli-wip/nix-homebrew";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    nix-snapd = {
+      url = "github:nix-community/nix-snapd";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+    apple-silicon = {
+      url = "github:tpwrules/nixos-apple-silicon";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
-  outputs = { self, ... } @ inputs:
+  outputs =
+    { self, ... }@inputs:
     let
       sources = import ./nix/sources.nix;
       nixpkgs = inputs.nixpkgs-unstable;
       lib = utils.extendLib nixpkgs.lib;
-      pkgs = utils.mkPkgs {};
+      pkgs = utils.mkPkgs { };
       pkgsLinux = utils.mkPkgs { system = "aarch64-linux"; };
 
       vars = {
@@ -37,151 +59,232 @@
       };
 
       utils = {
-        attrsToValues = attrs:
-          lib.attrsets.mapAttrsToList (name: value: value) attrs;
+        attrsToValues = attrs: lib.attrsets.mapAttrsToList (name: value: value) attrs;
 
-        extendLib = lib: lib.extend(self: super: {
-          hm = inputs.home-manager.lib.hm;
-          vmHostAttrs = options: block: if (builtins.hasAttr "cores" options.virtualisation) then block else {};
-          buildQemuVm = { name, targetSystem, configuration }:
-            (utils.mkVm { inherit name targetSystem configuration; }).config.system.build.startVm;
-        });
-
-        mkPkgs = { system ? vars.currentSystem, nixpkgs ? inputs.nixpkgs-unstable } : import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [
-            inputs.emacs-overlay.overlay
-            inputs.apple-silicon.overlays.apple-silicon-overlay
-            inputs.nix-alien.overlays.default
-            (_: super: self.packages."${system}")
-          ] ++ utils.attrsToValues self.overlays;
-        };
-
-        callPkg = package:
-          pkgs.callPackage package { inherit sources; };
-
-        mkHomeManagerModule = { name, version ? versions.homeManager.stateVersion }: {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = {
-              systemName = name;
-              inherit lib;
-              pkgsStable = utils.mkPkgs { nixpkgs = inputs.nixos-stable; };
-            };
-            sharedModules = [
-              { home.stateVersion = version; }
-            ] ++ utils.attrsToValues self.homeManagerModules;
-          };
-        };
-
-        mkVm = {
-          name,
-          targetSystem ? vars.currentSystem,
-          selfReference ? self,
-          nixpkgs ? inputs.nixpkgs-unstable,
-          hostPkgs ? pkgs,
-          profile ? ./profiles/nixos/qemu-vm.nix,
-          configuration ? { imports = [ (./vms/nixos + "/${name}.nix") ]; }
-        } : lib.nixosSystem {
-          specialArgs = {
-            inherit vars versions nixpkgs;
-            self = selfReference;
-            systemName = name;
-            pkgsStable = utils.mkPkgs { system = targetSystem; nixpkgs = inputs.nixos-stable; };
-          };
-          modules = [
-            profile
-            ({ lib, options, ... }:  {
-              networking.hostName = lib.mkDefault name;
-              nixpkgs.pkgs = utils.mkPkgs { system = targetSystem; inherit nixpkgs; };
-              modules.qemuGuest.enable = true;
-              virtualisation = lib.vmHostAttrs options {
-                host.pkgs = hostPkgs;
-              };
-
-              system = {
-                inherit name;
-                stateVersion = versions.nixos.stateVersion;
-                configurationRevision = versions.rev;
-              };
-
-              users.users.root.openssh.authorizedKeys.keyFiles = [ vars.sshKeyFile ];
-            })
-            inputs.home-manager.nixosModules.home-manager (utils.mkHomeManagerModule { inherit name; })
-            inputs.nix-snapd.nixosModules.default
-            configuration
-          ] ++ utils.attrsToValues self.nixosModules;
-        };
-
-        mkDocker = {
-          name,
-          targetSystem ? vars.currentSystem,
-          selfReference ? self,
-          configuration ? { imports = [ (./vms/nixos + "/${name}.nix") ]; }
-        } : lib.nixosSystem {
-          specialArgs = {
-            inherit vars versions nixpkgs;
-            self = selfReference;
-            systemName = name;
-            pkgsStable = utils.mkPkgs { system  = targetSystem; nixpkgs = inputs.nixos-stable; };
-          };
-          modules = [
-            ./profiles/nixos/docker-tar.nix
-            ({ lib, options, ... }:  {
-              networking.hostName = lib.mkDefault name;
-              nixpkgs.pkgs = (utils.mkPkgs { system = targetSystem; });
-
-              system = {
-                inherit name;
-                stateVersion = versions.nixos.stateVersion;
-                configurationRevision = versions.rev;
-              };
-            })
-            inputs.home-manager.nixosModules.home-manager (utils.mkHomeManagerModule { inherit name; })
-            configuration
-          ] ++ utils.attrsToValues self.nixosModules;
-        };
-
-        mkDarwin = { name }: inputs.nix-darwin.lib.darwinSystem {
-          specialArgs = {
-            inherit self vars versions nixpkgs;
-            systemName = name;
-            # pkgsStable = utils.mkPkgs { nixpkgs = inputs.nixos-stable; };
-          };
-          modules = [
-            { nixpkgs.pkgs = pkgs; }
-            inputs.home-manager.darwinModules.home-manager (utils.mkHomeManagerModule { inherit name; })
-            inputs.nix-homebrew.darwinModules.nix-homebrew
-            {
-              nix-homebrew = {
-                enable = true;
-                enableRosetta = false;
-                user = vars.primaryUser;
-              };
-
-              system = {
-                stateVersion = versions.darwin.stateVersion;
-                configurationRevision = versions.rev;
-              };
+        extendLib =
+          lib:
+          lib.extend (
+            self: super: {
+              hm = inputs.home-manager.lib.hm;
+              vmHostAttrs =
+                options: block: if (builtins.hasAttr "cores" options.virtualisation) then block else { };
+              buildQemuVm =
+                {
+                  name,
+                  targetSystem,
+                  configuration,
+                }:
+                (utils.mkVm { inherit name targetSystem configuration; }).config.system.build.startVm;
             }
-            (./hosts/darwin + "/${name}.nix")
-          ] ++ utils.attrsToValues self.darwinModules;
-        };
+          );
+
+        mkPkgs =
+          {
+            system ? vars.currentSystem,
+            nixpkgs ? inputs.nixpkgs-unstable,
+          }:
+          import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [
+              inputs.emacs-overlay.overlay
+              inputs.apple-silicon.overlays.apple-silicon-overlay
+              inputs.nix-alien.overlays.default
+              (_: super: self.packages."${system}")
+            ]
+            ++ utils.attrsToValues self.overlays;
+          };
+
+        callPkg = package: pkgs.callPackage package { inherit sources; };
+
+        mkHomeManagerModule =
+          {
+            name,
+            version ? versions.homeManager.stateVersion,
+          }:
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = {
+                systemName = name;
+                inherit lib;
+                pkgsStable = utils.mkPkgs { nixpkgs = inputs.nixos-stable; };
+              };
+              sharedModules = [
+                { home.stateVersion = version; }
+              ]
+              ++ utils.attrsToValues self.homeManagerModules;
+            };
+          };
+
+        mkVm =
+          {
+            name,
+            targetSystem ? vars.currentSystem,
+            selfReference ? self,
+            nixpkgs ? inputs.nixpkgs-unstable,
+            hostPkgs ? pkgs,
+            profile ? ./profiles/nixos/qemu-vm.nix,
+            configuration ? {
+              imports = [ (./vms/nixos + "/${name}.nix") ];
+            },
+          }:
+          lib.nixosSystem {
+            specialArgs = {
+              inherit vars versions nixpkgs;
+              self = selfReference;
+              systemName = name;
+              pkgsStable = utils.mkPkgs {
+                system = targetSystem;
+                nixpkgs = inputs.nixos-stable;
+              };
+            };
+            modules = [
+              profile
+              (
+                { lib, options, ... }:
+                {
+                  networking.hostName = lib.mkDefault name;
+                  nixpkgs.pkgs = utils.mkPkgs {
+                    system = targetSystem;
+                    inherit nixpkgs;
+                  };
+                  modules.qemuGuest.enable = true;
+                  virtualisation = lib.vmHostAttrs options {
+                    host.pkgs = hostPkgs;
+                  };
+
+                  system = {
+                    inherit name;
+                    stateVersion = versions.nixos.stateVersion;
+                    configurationRevision = versions.rev;
+                  };
+
+                  users.users.root.openssh.authorizedKeys.keyFiles = [ vars.sshKeyFile ];
+                }
+              )
+              inputs.home-manager.nixosModules.home-manager
+              (utils.mkHomeManagerModule { inherit name; })
+              inputs.nix-snapd.nixosModules.default
+              configuration
+            ]
+            ++ utils.attrsToValues self.nixosModules;
+          };
+
+        mkDocker =
+          {
+            name,
+            targetSystem ? vars.currentSystem,
+            selfReference ? self,
+            configuration ? {
+              imports = [ (./vms/nixos + "/${name}.nix") ];
+            },
+          }:
+          lib.nixosSystem {
+            specialArgs = {
+              inherit vars versions nixpkgs;
+              self = selfReference;
+              systemName = name;
+              pkgsStable = utils.mkPkgs {
+                system = targetSystem;
+                nixpkgs = inputs.nixos-stable;
+              };
+            };
+            modules = [
+              ./profiles/nixos/docker-tar.nix
+              (
+                { lib, options, ... }:
+                {
+                  networking.hostName = lib.mkDefault name;
+                  nixpkgs.pkgs = (utils.mkPkgs { system = targetSystem; });
+
+                  system = {
+                    inherit name;
+                    stateVersion = versions.nixos.stateVersion;
+                    configurationRevision = versions.rev;
+                  };
+                }
+              )
+              inputs.home-manager.nixosModules.home-manager
+              (utils.mkHomeManagerModule { inherit name; })
+              configuration
+            ]
+            ++ utils.attrsToValues self.nixosModules;
+          };
+
+        mkDarwin =
+          { name }:
+          inputs.nix-darwin.lib.darwinSystem {
+            specialArgs = {
+              inherit
+                self
+                vars
+                versions
+                nixpkgs
+                ;
+              systemName = name;
+              # pkgsStable = utils.mkPkgs { nixpkgs = inputs.nixos-stable; };
+            };
+            modules = [
+              { nixpkgs.pkgs = pkgs; }
+              inputs.home-manager.darwinModules.home-manager
+              (utils.mkHomeManagerModule { inherit name; })
+              inputs.nix-homebrew.darwinModules.nix-homebrew
+              {
+                nix-homebrew = {
+                  enable = true;
+                  enableRosetta = false;
+                  user = vars.primaryUser;
+                };
+
+                system = {
+                  stateVersion = versions.darwin.stateVersion;
+                  configurationRevision = versions.rev;
+                };
+              }
+              (./hosts/darwin + "/${name}.nix")
+            ]
+            ++ utils.attrsToValues self.darwinModules;
+          };
       };
     in
     {
       inherit utils;
 
-      nixosConfigurations.playground = utils.mkVm { name = "playground"; targetSystem = "aarch64-linux"; };
-      nixosConfigurations.toolbox = utils.mkVm { name = "toolbox"; targetSystem = "aarch64-linux"; };
-      nixosConfigurations.docker = utils.mkVm { name = "docker"; targetSystem = "aarch64-linux"; };
-      nixosConfigurations.gnome = utils.mkVm { name = "gnome"; targetSystem = "aarch64-linux"; };
-      nixosConfigurations.firefox = utils.mkVm { name = "firefox"; targetSystem = "aarch64-linux"; };
-      nixosConfigurations.k3s = utils.mkVm { name = "k3s"; targetSystem = "aarch64-linux"; };
-      nixosConfigurations.snapd = utils.mkVm { name = "snapd"; targetSystem = "aarch64-linux"; };
-      nixosConfigurations.playground-qcow2 = utils.mkVm { name = "playground"; targetSystem = "aarch64-linux"; profile = ./profiles/nixos/qemu-qcow2.nix; };
+      nixosConfigurations.playground = utils.mkVm {
+        name = "playground";
+        targetSystem = "aarch64-linux";
+      };
+      nixosConfigurations.toolbox = utils.mkVm {
+        name = "toolbox";
+        targetSystem = "aarch64-linux";
+      };
+      nixosConfigurations.docker = utils.mkVm {
+        name = "docker";
+        targetSystem = "aarch64-linux";
+      };
+      nixosConfigurations.gnome = utils.mkVm {
+        name = "gnome";
+        targetSystem = "aarch64-linux";
+      };
+      nixosConfigurations.firefox = utils.mkVm {
+        name = "firefox";
+        targetSystem = "aarch64-linux";
+      };
+      nixosConfigurations.k3s = utils.mkVm {
+        name = "k3s";
+        targetSystem = "aarch64-linux";
+      };
+      nixosConfigurations.snapd = utils.mkVm {
+        name = "snapd";
+        targetSystem = "aarch64-linux";
+      };
+      nixosConfigurations.playground-qcow2 = utils.mkVm {
+        name = "playground";
+        targetSystem = "aarch64-linux";
+        profile = ./profiles/nixos/qemu-qcow2.nix;
+      };
 
       darwinConfigurations."bootstrap" = utils.mkDarwin { name = "bootstrap"; };
       darwinConfigurations."m4" = utils.mkDarwin { name = "m4"; };
@@ -190,8 +293,12 @@
         aarch64-darwin = {
           # options.json
           home-manager-options-json = inputs.home-manager.packages.aarch64-darwin.docs-json;
-          nixos-options-json = (lib.nixosSystem { modules = [ { nixpkgs.pkgs = pkgs; } ]; }).config.system.build.manual.optionsJSON;
-          darwin-options-json = (inputs.nix-darwin.lib.darwinSystem { modules = [ { nixpkgs.pkgs = pkgs; } ]; }).config.system.build.manual.optionsJSON;
+          nixos-options-json =
+            (lib.nixosSystem { modules = [ { nixpkgs.pkgs = pkgs; } ]; })
+            .config.system.build.manual.optionsJSON;
+          darwin-options-json =
+            (inputs.nix-darwin.lib.darwinSystem { modules = [ { nixpkgs.pkgs = pkgs; } ]; })
+            .config.system.build.manual.optionsJSON;
           # VMs
           playground-vm = self.nixosConfigurations.playground.config.system.build.startVm;
           toolbox-vm = self.nixosConfigurations.toolbox.config.system.build.startVm;
@@ -218,7 +325,11 @@
             copyChannel = false;
           };
           # Docker images
-          playground-docker = (utils.mkDocker { name = "playground"; targetSystem = "aarch64-linux"; }).config.system.build.tarball;
+          playground-docker =
+            (utils.mkDocker {
+              name = "playground";
+              targetSystem = "aarch64-linux";
+            }).config.system.build.tarball;
           # Packages
           macos = {
             socket_vmnet = (utils.callPkg ./pkgs/darwin/applications/virtualization/socket_vmnet.nix);
@@ -238,7 +349,6 @@
         hunspell = import ./modules/darwin/hunspell.nix;
         nix = import ./modules/darwin/nix.nix;
         socket-vmnet = import ./modules/darwin/socket-vmnet.nix;
-        tuptime = import ./modules/darwin/tuptime.nix;
       };
 
       homeManagerModules = {
